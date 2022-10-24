04fb94f8874d768a17f9266536dbd35c7a673d02
Alexey <slow.net@mail.ru>
2022-10-24 13:27:45 +0300

    Решение 1-ой задачи ДЗ семинара 3

---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int fiveDigit = Convert.ToInt32(Console.ReadLine());
+
+if (fiveDigit < 10000 || fiveDigit > 99999)
+{
+    Console.WriteLine("Введенное число не является пятизначным");
+}
+else
+{
+    int[] array = new int[5];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        int LastNumber = fiveDigit % 10;
+        array[i] = LastNumber;
+        fiveDigit = fiveDigit / 10;
+    }
+    int count =0;
+    for (int i = 0; i < array.Length/2; i++)
+    {
+        if (array[i] == array[array.Length - 1 - i])
+        {
+           count++;
+        }
+    }
+if( count==array.Length/2)
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+Console.WriteLine("Число не является палиндромом");
+}
