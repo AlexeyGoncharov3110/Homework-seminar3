5b829596d1eaac67b8477bbc70835e5b6b4d2cb7
Alexey <slow.net@mail.ru>
2022-10-24 12:08:11 +0300

    Решение 3-ей задачи ДЗ семинара 3

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] GetTableOfCubes( int n)
+{
+int [] array = new int [n];
+for( int i = 0 ; i < array.Length; i++)
+{
+    array [i] = (i + 1) * (i + 1) * (i + 1);
+}
+return array ;
+}
+
+int [] result = GetTableOfCubes(n);
+
+PrintArray(result);
+
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
